44d522cb4cd3d586cee90b562a03f686f11c4246
Leila Moraes <leila.violino@gmail.com>
2023-03-03 17:20:00 -0300

    feat: cria migration create-postsCategories

--- src/migrations/20230303201026-create-postsCategories.js
+++ src/migrations/20230303201026-create-postsCategories.js
@@ -2,20 +2,36 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        field: 'post_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        }
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        field: 'category_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      },
+    });
+    
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
   }
 };
