2c259d7bbe1f5eecefa4a276b0c3dacc86638057
Leila Moraes <leila.violino@gmail.com>
2023-03-07 03:13:25 -0300

    fix: corrige o nome da tabela no model Category

--- src/models/Category.js
+++ src/models/Category.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       {
         timestamps: false,
-        tableName: 'category',
+        tableName: 'categories',
       });
     return Category;
   };
