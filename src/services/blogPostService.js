dec5a8c6ffe7943dd79870c0f005d803d97338cd
Leila Moraes <leila.violino@gmail.com>
2023-03-07 18:16:06 -0300

    feat: função service para retornar um post do blog

--- src/services/blogPostService.js
+++ src/services/blogPostService.js
@@ -1,6 +1,8 @@
 const { BlogPost, User, Category } = require('../models');
 
 const OK = 200;
+const NOT_FOUND = 404;
+const MESSAGE = { message: 'Post does not exist' };
 
 const getPosts = async () => {
   const posts = await BlogPost.findAll({ include: [
@@ -9,4 +11,12 @@ const getPosts = async () => {
   return { type: OK, message: posts };
 };
 
-module.exports = { getPosts };
+const getPost = async (id) => {
+  const post = await BlogPost.findByPk(id, { include: [
+    { model: User, as: 'user', attributes: { exclude: 'password' } },
+    { model: Category, as: 'categories', through: { attributes: [] } }] });
+  if (!post) return { type: NOT_FOUND, message: MESSAGE };
+  return { type: OK, message: post };
+};
+
+module.exports = { getPosts, getPost };
