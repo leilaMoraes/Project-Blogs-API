7b6b4ed975baada674cf088b7edff2c1f4b47dc4
Leila Moraes <leila.violino@gmail.com>
2023-03-07 18:00:45 -0300

    feat: atualiza index da pasta controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const loginController = require('./loginController');
 const userController = require('./userController');
 const categoryController = require('./categoryController');
+const blogPostController = require('./blogPostController');
 
-module.exports = { loginController, userController, categoryController };
+module.exports = { loginController, userController, categoryController, blogPostController };
