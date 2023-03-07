af5369031dfaa10b107feb7d2c64163236cb74c8
Leila Moraes <leila.violino@gmail.com>
2023-03-07 17:58:51 -0300

    feat: atualiza index da pasta routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const loginRouter = require('./loginRoutes');
 const userRouter = require('./userRoutes');
 const categoryRouter = require('./categoryRoutes');
+const postRouter = require('./blogPostRoutes');
 
-module.exports = { loginRouter, userRouter, categoryRouter };
+module.exports = { loginRouter, userRouter, categoryRouter, postRouter };
