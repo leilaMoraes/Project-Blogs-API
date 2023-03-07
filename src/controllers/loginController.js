3beafbec28f8720eace71565a9bb5cbc22ae6883
Leila Moraes <leila.violino@gmail.com>
2023-03-06 22:11:27 -0300

    feat: função controller para registrar login

---
+++ src/controllers/loginController.js
@@ -0,0 +1,16 @@
+const loginService = require('../services');
+
+const createToken = require('../auth/authToken');
+
+module.exports = async (req, res) => {
+  try {
+  const { email, password } = req.body;
+  const { type, message } = await loginService.loginService(email, password);
+  if (type === 200) {
+    const token = createToken.createToken(email);
+    return res.status(type).json({ token });
+  } return res.status(type).json({ message });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
