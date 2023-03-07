2e65a12044676a866f1849dd157ed594b0aa49af
Leila Moraes <leila.violino@gmail.com>
2023-03-07 02:26:03 -0300

    feat: model BlogPost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define( 'BlogPost', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+    },
+    {
+      tableName: 'blog_posts',
+      underscored: true,
+      createdAt: 'published',
+      updatedAt: 'updated',
+    },);
+
+    BlogPost.associate = (model) => {
+      BlogPost.belongsTo(model.User,
+        { foreignKey: 'user_id', as: 'user'});
+    };
+
+  return BlogPost;
+};
