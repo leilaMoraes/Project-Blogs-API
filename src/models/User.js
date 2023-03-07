e5c875532d62a525bc3efeac86318a1fb7727b30
Leila Moraes <leila.violino@gmail.com>
2023-03-07 03:42:23 -0300

    fix: espaçamento das models

--- src/models/User.js
+++ src/models/User.js
@@ -1,21 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define( 'User', {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      displayName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      image: DataTypes.STRING,
-    },
-    {
-      timestamps: false,
-      tableName: 'users',
-      underscored: true,
-    });
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true
+  });
 
-    User.associate = (model) => {
-      User.hasMany(model.BlogPost,
-        { foreignKey: 'userId', as: 'posts'});
-    };
+  User.associate = (model) => {
+    User.hasMany(model.BlogPost,
+      { foreignKey: 'userId', as: 'posts'});
+  };
   
   return User;
 };
