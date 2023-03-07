8998efa2ade120812cdd5c171d9814e102005b3a
Leila Moraes <leila.violino@gmail.com>
2023-03-06 22:58:04 -0300

    feat: função para validar o email do usuário

--- src/middleware/userValidation.js
+++ src/middleware/userValidation.js
@@ -1,3 +1,5 @@
+const { User } = require('../models');
+
 const nameValidation = (req, res, next) => {
   const { displayName } = req.body;
   if (displayName.length < 8) {
@@ -8,4 +10,16 @@ const nameValidation = (req, res, next) => {
   return next();
 };
 
-module.exports = { nameValidation };
+const emailValidation = async (req, res, next) => {
+  const { email } = req.body;
+  const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i;
+  if (!emailRegex.test(email)) {
+    return res.status(400)
+    .json({ message: '"email" must be a valid email' });
+  }
+  const userRegistered = await User.findOne({ where: { email } });
+  if (userRegistered) return res.status(409).json({ message: 'user already registered' });
+  return next();
+};
+
+module.exports = { nameValidation, emailValidation };
