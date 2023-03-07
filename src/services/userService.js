19e8346d169cfbfbb74ff0917749bac8df3880c9
Leila Moraes <leila.violino@gmail.com>
2023-03-06 23:25:20 -0300

    fix: troca name por displayName na funçãa insertUser no service

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,8 +2,8 @@ const { User } = require('../models');
 
 const CREATED = 201;
 
-const insertUser = async (name, email, password, image) => {
-  const newUser = await User.create({ name, email, password, image });
+const insertUser = async (displayName, email, password, image) => {
+  const newUser = await User.create({ displayName, email, password, image });
   if (newUser) return { type: CREATED };
 };
 
