83c96a5e2b9e4286c1cecd5012c4bf9f719b6635
Leila Moraes <leila.violino@gmail.com>
2023-03-06 23:17:29 -0300

    fix: importação de loginService no loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,11 +1,11 @@
-const loginService = require('../services');
+const { loginService } = require('../services');
 
 const createToken = require('../auth/authToken');
 
 module.exports = async (req, res) => {
   try {
   const { email, password } = req.body;
-  const { type, message } = await loginService.loginService(email, password);
+  const { type, message } = await loginService(email, password);
   if (type === 200) {
     const token = createToken.createToken(email);
     return res.status(type).json({ token });
