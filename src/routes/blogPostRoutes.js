b6774573ad7de1d9715d8dd41bd7e322eb8a8669
Leila Moraes <leila.violino@gmail.com>
2023-03-08 01:11:21 -0300

    feat: rota para inserir novo post

--- src/routes/blogPostRoutes.js
+++ src/routes/blogPostRoutes.js
@@ -5,6 +5,7 @@ const postRouter = express.Router();
 const { blogPostController } = require('../controllers');
 const tokenValidation = require('../middleware/tokenValidation');
 
+postRouter.post('/', tokenValidation, blogPostController.insertPost);
 postRouter.get('/', tokenValidation, blogPostController.getPosts);
 postRouter.get('/:id', tokenValidation, blogPostController.getPost);
 
